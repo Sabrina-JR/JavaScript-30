e676582da7a55c94311a8e2ef402c95162adb673
Sabrina <sabrina19960226@gmail.com>
2021-06-30 19:45:10 +0800

    -Drum-Kit- Add keyup function

--- 01_Java-Script-Drum-Kit/main.js
+++ 01_Java-Script-Drum-Kit/main.js
@@ -1,3 +1,9 @@
+   
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+}
+
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
@@ -7,5 +13,8 @@ function playSound(e) {
     audio.currentTime = 0;
     audio.play();
    }
+
+const keys = Array.from(document.querySelectorAll('.key'));   
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
 window.addEventListener('keydown', playSound);
